b72671824a08dfde8674b335ab5f3a9c8a9057c0
Phu Vuong <phuvuong@kms-technology.com>
2022-08-02 23:43:25 +0700

    apply unittest

--- Tests/DemoQA/ElementTests.py
+++ Tests/DemoQA/ElementTests.py
@@ -6,24 +6,36 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 import time
+import unittest
 
-options = Options()
-options.add_argument("start-maximized")
-service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service, options=options)
+class ElementTest(unittest.TestCase):
+    def setUp(self):
+        options = Options()
+        options.add_argument("start-maximized")
+        service = Service(ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(service=service, options=options)
+        self.driver.get("https://demoqa.com/")
 
-driver.get("https://demoqa.com/")
+    def tearDown(self):
+        self.driver.close()
 
-#actions = ActionChains(driver)
-#actions.move_to_element(driver.find_element(By.XPATH, "//h5[text()='Elements']")).perform()
+    def testElement(self):
+        # actions = ActionChains(driver)
+        # actions.move_to_element(driver.find_element(By.XPATH, "//h5[text()='Elements']")).perform()
 
-#html = driver.find_element(By.TAG_NAME, 'html')
-#html.send_keys(Keys.END)
+        # html = driver.find_element(By.TAG_NAME, 'html')
+        # html.send_keys(Keys.END)
 
-driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-time.sleep(5)
+        time.sleep(5)
 
-driver.find_element(By.XPATH, "//h5[text()='Elements']").click()
+        self.driver.find_element(By.XPATH, "//h5[text()='Elements']").click()
 
-driver.close()
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+
