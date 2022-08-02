ce7c66a7bed5cb61cbf77919782af077dcd5a4b5
Phu Vuong <phuvuong@kms-technology.com>
2022-08-03 00:05:47 +0700

    Refactor names

--- tests/demo_qa/element_tests.py
+++ tests/demo_qa/element_tests.py
@@ -7,10 +7,10 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 import time
 import unittest
-from Main.Base.BaseTest import BaseTest
+from src.base.base_test import BaseTest
 
 class ElementTest(BaseTest):
-    def testElement(self):
+    def test_element(self):
         # actions = ActionChains(driver)
         # actions.move_to_element(driver.find_element(By.XPATH, "//h5[text()='Elements']")).perform()
 
