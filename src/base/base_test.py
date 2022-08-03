b6d782c33404804a9ac28a071255f3ac77fa5576
Phu Vuong <phuvuong@kms-technology.com>
2022-08-03 23:45:56 +0700

    Remove unused imports

--- src/base/base_test.py
+++ src/base/base_test.py
@@ -2,10 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
-import time
 import unittest
 
 class BaseTest(unittest.TestCase):
